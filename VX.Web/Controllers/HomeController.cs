b1b44915b1b3e5001206eb1af146a95046b99999
Evgeny Skurikhin <eskurihin@gmail.com>
2012-04-11 08:11:56 +0400

    Solution added

---
+++ VX.Web/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+using VX.Web.VocabExtServiceReference;
+
+namespace VX.Web.Controllers
+{
+    [HandleError]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var service = new VocabExtServiceClient();
+            ViewData["Message"] = service.GetData(1);
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
