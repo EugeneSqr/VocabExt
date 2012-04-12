ef10aa4033a50243d78ae944481b85c09625bbc7
Evgeny Skurikhin <eskurihin@gmail.com>
2012-04-12 21:04:58 +0400

    Desktop consumer added

---
+++ VX.Desktop/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using VX.Domain;
+using VX.Domain.Interfaces;
+
+namespace VX.Desktop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            IList<IWord> words = new List<IWord>();
+            words.Add(new Word 
+            { 
+                Spelling = "Cat",
+                Transcription = "Cat"
+            });
+            words.Add(new Word
+            {
+                Spelling = "Dog",
+                Transcription = "Dog"
+            });
+            words.Add(new Word
+            {
+                Spelling = "Cow",
+                Transcription = "Cow"
+            });
+            this.wordsList.ItemsSource = words;
+        }
+    }
+}
